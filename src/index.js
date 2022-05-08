5225f420a0e26936386affeed14cf01247507b9c
Xiao <xmeng1@ualberta.ca>
2022-05-08 11:13:55 -0600

    set up styles.css with import connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as homePage from './home-page.js';
 import * as menuPage from './menu-page.js';
 import * as contactPage from './contact-page.js';
+import './styles.css';
 
 let pages = {homePage, menuPage, contactPage};
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  watch: true,
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
 };
